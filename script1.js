3f7b7f7a862d1bf54c8cc8f850150eb399796d7d
jocey <lostjoce@gmail.com>
2018-12-06 18:08:44 +1300

    added about section
    edited the script to show and hide the text when the about button is clicked
    also tweaked mobile view height

--- script1.js
+++ script1.js
@@ -1,18 +1,24 @@
 // essentially the w3s gallery script
 function expand(imgs) {
+    // reset the .display properties
+    document.getElementById("expandedPhoto").style.display="initial"
+    document.getElementById("caption").style.display="block"
+    
+    // image changing script
     var expandImg = document.getElementById("expandedPhoto");
     var imgText = document.getElementById("caption");
     expandImg.src = imgs.src;
     imgText.innerHTML = imgs.alt;
     expandImg.parentElement.style.display = "block";
+    imgText.parentElement.style.display = "block";
+    document.getElementById("aboutText").style.display="none"
+    
 }
 
 // remove any photo and caption currently displayed
-// and replace it with text ??
+// and replace it with the aboutText
 function showAbout() {
-    var expandImg = document.getElementById("expandedPhoto");
-    expandImg.parentElement.style.display = "none";
-    
-/*    var txt = document.getElementById("aboutText");
-    document.getElementById("caption")innerHTML= "txt";*/
+    document.getElementById("expandedPhoto").style.display="none"
+    document.getElementById("caption").style.display="none"
+    document.getElementById("aboutText").style.display="block"
 }

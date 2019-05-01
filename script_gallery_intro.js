9a09cc053c89e28d366bdb83fd7af2df8b8bc396
threetimesover <45932009+threetimesover@users.noreply.github.com>
2019-05-01 21:44:52 +1200

    Add files via upload

--- script_gallery_intro.js
+++ script_gallery_intro.js
@@ -14,6 +14,7 @@ function expand(imgs) {
     imgText.parentElement.style.display = "block";
 }
 
+
 // remove any photo and caption currently displayed
 // and replace it with the aboutText
 function showAbout() {
@@ -37,4 +38,16 @@ else if (document.getElementById("expandedPhoto").style.height === "88vh")
  {document.getElementById("expandedPhoto").style.height = "auto";}
 
 else {document.getElementById("expandedPhoto").style.height = "88vh";}
+}
+
+
+function cursor() {
+
+if (window.matchMedia("(max-aspect-ratio: 5/4)").matches)
+ {return;}
+
+else if (document.getElementById("expandedPhoto").style.height === "auto")
+ {document.getElementById("expandedPhoto").style.cursor = "zoom-out;"}
+
+else {document.getElementById("expandedPhoto").style.cursor = "zoom-in;"}
 }
